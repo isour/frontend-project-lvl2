a554be45d8db2d784e93e486bd1b23b402c78763
isour <admin@isour.ru>
2021-09-01 09:42:02 +0800

    step 1

---
+++ bin/nodejs-package.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import program from "commander";
+import gendiff from "../src/gendiff";
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+const packageFile = require("../package.json");
+   
+program
+    .version(packageFile.version)
+    .description(packageFile.description)
+    .usage("[options]")
+    .parse(process.argv);
