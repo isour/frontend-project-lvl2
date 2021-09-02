7c49470ba5c5d7bae7d092e8f4d4d93c13a93548
isour <admin@isour.ru>
2021-09-02 10:33:50 +0800

    step 3

--- bin/nodejs-package.js
+++ bin/nodejs-package.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
 import program from "commander";
-// import gendiff from "../src/gendiff";
+import * as fs from 'fs';
 import { createRequire } from "module";
+import gendiff from "../src/gendiff.js";
 
 const require = createRequire(import.meta.url);
 const packageFile = require("../package.json");
@@ -11,5 +12,11 @@ program
     .version(packageFile.version)
     .description(packageFile.description)
     .usage("[options]")
-    .option('-f, --format [type]', 'output format')
-    .parse(process.argv);
+    .option('-f, --format <type>', 'output format')
+    .action(() => {
+        const file1 = fs.readFileSync(program.args[0]);
+        const file2 = fs.readFileSync(program.args[1]);
+
+        gendiff(JSON.parse(file1.toString()), JSON.parse(file2.toString()));
+    })
+    .parse();
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,3 +1,29 @@
-export default () => {
+import _ from 'lodash';
 
+export default (object1, object2) => {
+    const sortedObject1 = _(object1).toPairs().sortBy(0).fromPairs().value();
+    const sortedObject2 = _(object2).toPairs().sortBy(0).fromPairs().value();
+    const allKeys = _.union(Object.keys(sortedObject1), Object.keys(sortedObject2));
+    allKeys.forEach(function(objectKey) {
+        if (sortedObject1[objectKey] === sortedObject2[objectKey]) {
+            console.log(`  ${objectKey}: '${sortedObject1[objectKey]}'`)
+            return;
+        }
+        if (sortedObject1[objectKey] !== sortedObject2[objectKey]) {
+            console.log(`- ${objectKey}: '${sortedObject1[objectKey]}'`)
+            console.log(`+ ${objectKey}: '${sortedObject2[objectKey]}'`)
+            return;
+        }
+
+        if (!sortedObject2.hasOwnProperty(objectKey)) {
+            console.log(`- ${objectKey}: '${sortedObject1[objectKey]}'`)
+            return;
+        }
+
+        if (!sortedObject1.hasOwnProperty(objectKey)) {
+            console.log(`+ ${objectKey}: '${sortedObject2[objectKey]}'`)
+            return;
+        }
+    })
+    console.log('}');
 }
