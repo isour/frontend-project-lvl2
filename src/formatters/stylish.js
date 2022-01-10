30d74db16b0f9042629730818872c932d658bb85
isour <admin@isour.ru>
2022-01-10 14:36:29 +0800

    linter test

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,6 +1,4 @@
-const replaceAt = (str, index, rpc) => {
-  return str.substr(0, index) + rpc + str.substr(index + rpc.length)
-};
+const replaceAt = (str, index, rpc) => str.substr(0, index) + rpc + str.substr(index + rpc.length);
 
 const getIndentation = (depth, symb = ' ') => {
   const emptyString = '    '.repeat(depth);
